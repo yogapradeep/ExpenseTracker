c374d296e2b454faee8761f06edc400edc48cbeb
yogapradeep <v.yogapradeep@gmail.com>
2024-05-21 22:50:35 +0530

    Initial commit - added neccessary packages

---
+++ app/expenses/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "react-bootstrap";
+
+function page() {
+  return (
+    <div>
+      <p>expense page</p>
+      <Button>test react bootstrap</Button>
+    </div>
+  );
+}
+
+export default page;
